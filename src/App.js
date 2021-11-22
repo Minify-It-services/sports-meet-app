be41f84afb7088d55b5a54dce26d4b35e11607e3
Alson33 <garbuja12alson@gmail.com>
2021-11-22 23:40:56 +0545

    add routing for team registration

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import PhoneRegister from './pages/PhoneRegister/index'
-import Login from './pages/Login/index'
-import Profile from './pages/Profile/index'
+import { 
+    SportPage, HomePage, SoloRegistration, 
+    TeamRegistration, DuoRegistration,
+    PhoneRegister, ProfilePage, Login
+  } from './pages'
 
 import React from 'react';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import TeamRegistration from './pages/TeamRegistration';
 import './App.css';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/phoneRegister" element={<PhoneRegister />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/phone-register" element={<PhoneRegister />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/teamRegister/*" element={<Team />} />
       </Routes>
     </BrowserRouter>
   );
+}
+
+const Team = () => {
+  return (
+    <Routes>
+      <Route path="sportSelect" element={<SportPage />} />
+      <Route path="team-registration" element={<TeamRegistration />} />
+      <Route path="duo-registration" element={<DuoRegistration />} />
+      <Route path="solo-registration" element={<SoloRegistration />} />
+    </Routes>
+  )
 }
 
 export default App;
