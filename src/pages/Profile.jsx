a577d81d7849f49d44549ddae7f0669b069fd4ab
Alson33 <garbuja12alson@gmail.com>
2021-11-25 11:45:19 +0545

    Made Profile page dynamic
    - Add layout to profile page
    - checking for token expiration as well as id
    - fetch user data

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,48 +1,85 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 import { Container, Box, Stack, Avatar, Typography, Grid, Card, CardContent } from '@mui/material';
 
 // data
-import { sports } from '../utils/datas/sportData';
+import Layout from '../layout/Layout';
 
 const Profile = () => {
 
+    const navigate = useNavigate()
+    
+    const {id} = JSON.parse(localStorage.getItem('user'))
+    const token = localStorage.getItem('token')
+    const [ user, setUser ] = useState({})
+
+    const getUserData = async () => {
+        let response = {}
+        await axios({
+            method: 'GET',
+            baseURL: process.env.REACT_APP_ENVIRONMENT === 'development'?'http://localhost:5000/v1':'Hosting URL',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+            url: `/users/${id}`,
+        }).then(res => response = res?.data.data)
+          .catch(err => response = err?.response.data)
+
+        if(response.message && response.message==='Please authenticate')
+            navigate('/login')
+
+        setUser(response)
+    }
+
+    useEffect(() => {
+        if(!id){
+            navigate('/login')
+        }
+        getUserData()
+    // eslint-disable-next-line
+    }, [])
+
     return (
-        <Box sx={{width:'100%',height:'100vh'}}>
-            <Container sx={{height:'100%', width:'100%', display:'flex', flexDirection:'column', justifyContent:'space-evenly'}}>
+        <Layout>
+            <Box sx={{width:'100%',height:'100vh'}}>
+                <Container sx={{height:'100%', width:'100%', display:'flex', flexDirection:'column', justifyContent:'space-evenly'}}>
 
-                <Stack direction="row" spacing={5} justifyContent="center" alignItems="center">
-                    <Avatar src="./images/profile.jpg" variant="rounded" sx={{width:'100px', height:'100px'}}/>
+                    <Stack direction="row" spacing={5} justifyContent="center" alignItems="center">
+                        <Avatar src="./images/profile.jpg" variant="rounded" sx={{width:'100px', height:'100px'}}/>
+                        <Box>
+                            <Typography variant="h6" sx={{ fontWeight: 'bold' }}>{user.name}</Typography>
+                            <Typography variant="caption" sx={{fontStyle: 'italic', fontWeight: 'regular'}}>{user.email}</Typography>
+                        </Box>
+                    </Stack>
+
                     <Box>
-                        <Typography variant="h6" sx={{ fontWeight: 'bold' }}>Sunil Paudel</Typography>
-                        <Typography variant="caption" sx={{fontStyle: 'italic', fontWeight: 'regular'}}>be2018se001@gces.edu.np</Typography>
+                        <Typography variant="h6" sx={{ fontWeight: 'bold', marginBottom:'25px' }}>Participated Sports:</Typography>
+                        <Grid container spacing={1}>
+                            {user?.teams?.map((sport)=>{
+                                
+                                const {id, typeOfSports, getIcon} = sport;
+
+                                return(
+                                    <Grid key={id} item xs={6} md={3} justifyContent="center" textAlign="center">
+                                        <Card>
+                                            <CardContent sx={{display:'grid', gridTemplateColumns:'20% 80%'}}>
+                                                <Box>{getIcon()}</Box>
+                                                <Typography variant="body1" sx={{display:'flex', justifyContent:'flex-start'}}>
+                                                    {typeOfSports}
+                                                </Typography>
+                                            </CardContent>
+                                        </Card>
+                                    </Grid>
+                                );
+                                
+                            })}
+                        </Grid>
                     </Box>
-                </Stack>
-
-                <Box>
-                    <Typography variant="h6" sx={{ fontWeight: 'bold', marginBottom:'25px' }}>Participated Sports:</Typography>
-                    <Grid container spacing={1}>
-                        {sports.map((sport)=>{
-                            
-                            const {id, typeOfSports, getIcon} = sport;
-
-                            return(
-                                <Grid key={id} item xs={6} md={3} justifyContent="center" textAlign="center">
-                                    <Card>
-                                        <CardContent sx={{display:'grid', gridTemplateColumns:'20% 80%'}}>
-                                            <Box>{getIcon()}</Box>
-                                            <Typography variant="body1" sx={{display:'flex', justifyContent:'flex-start'}}>
-                                                {typeOfSports}
-                                            </Typography>
-                                        </CardContent>
-                                    </Card>
-                                </Grid>
-                            );
-                            
-                        })}
-                    </Grid>
-                </Box>
-            </Container>
-        </Box>
+                </Container>
+            </Box>
+        </Layout>
     )
 }
 
