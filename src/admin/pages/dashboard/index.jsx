31aa127cb61505466480672d74f11713bedc22b3
Alson33 <garbuja12alson@gmail.com>
2021-11-26 20:00:57 +0545

    call counts api and authenticate admin

--- src/admin/pages/dashboard/index.jsx
+++ src/admin/pages/dashboard/index.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Cookies from 'universal-cookie';
+import { useNavigate } from 'react-router-dom'
 
 // components
 import DrawerBar from '../../../components/DrawerBar';
+import jsendDestructor from '../../../utils/api/jsendDestructor';
 
 const Dashboard = () => {
+
+    const navigate = useNavigate()
+    const cookies = new Cookies();
+    
+    const token = cookies.get('sports_app_token');
+    const [counts, setCounts] = useState({})
+
+    const jsendRes = new jsendDestructor({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+    })
+    
+    const getData = async () => {
+        const { data, status, message } = await jsendRes.destructFromApi('/admin/dashboard', 'GET')
+
+        if(status === 'success'){
+            setCounts(data)
+            console.log(counts);
+        }else{
+            if(message === 'Forbidden')
+                navigate('/profile')
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    // eslint-disable-next-line
+    }, [])
+
     return (
         <>
             <DrawerBar pageName={'Dashboard'} pageId={1} />
