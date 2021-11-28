374abe679dee0f3293f496f65c3846ad945844bf
Yogesh070 <ythapa430@gmail.com>
2021-11-28 15:50:23 +0545

    sports card added

--- src/components/CustomCard.jsx
+++ src/components/CustomCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import '../styles/customcard/customcard.scss'
+import Typography from '@mui/material/Typography';
+
 
 function CustomCard(props) {
     return (
         <div id="customcard">
             <div className="img-container"><img src={props.image} alt="icon" /></div>
-            <p id="label">{props.label}</p>
+            <Typography id="label" variant="body1" component="p">{props.label}</Typography>
         </div>
     )
 }
--- src/pages/TeamRegister.jsx
+++ src/pages/TeamRegister.jsx
@@ -1,73 +1,95 @@
-import React, { useState, useEffect } from 'react'
-import Layout from '../layout/Layout'
-import { Box, Container, Typography, Paper, Avatar } from '@mui/material'
-import { styled } from '@mui/material/styles';
-import jsendDestructor from '../utils/api/jsendDestructor'
-import Cookies from 'universal-cookie'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
 
-const Item = styled(Paper)(() => ({
-    textAlign: 'center',
-    height: 60,
-    lineHeight: '60px',
-  }));
+import CustomCard from "../components/CustomCard";
+import Layout from "../layout/Layout";
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import jsendDestructor from "../utils/api/jsendDestructor";
+import Cookies from "universal-cookie";
+import { Link } from "react-router-dom";
+
+// const Item = styled(Paper)(() => ({
+//     textAlign: "center",
+//     height: 60,
+//     lineHeight: "60px",
+// }));
 
 const TeamRegister = () => {
-
-    const cookies = new Cookies()
-    const token = cookies.get('sports_app_token')
+    const cookies = new Cookies();
+    const token = cookies.get("sports_app_token");
     const jsendRes = new jsendDestructor({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-    })
-    const [ sports, setSports ] = useState([])
-    const getSports = async ()=> {
-        const {data, status, message} = await jsendRes.destructFromApi('/sports', 'GET')
-        if(status === 'success'){
-            setSports(data)
-        }
-        else{
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+    });
+    const [sports, setSports] = useState([]);
+    const getSports = async () => {
+        const { data, status, message } = await jsendRes.destructFromApi(
+            "/sports",
+            "GET"
+        );
+        if (status === "success") {
+            setSports(data);
+        } else {
             console.log(data, message);
         }
-    }
+    };
     useEffect(() => {
-        getSports()
-    }, [])
+        getSports();
+    }, []);
 
     return (
-        <Layout title='Team Register'>
-            <Box sx={{ width: '100%' }}>
-                <Container sx={{height:'100%', width:'100%', display:'flex', flexDirection:'column', justifyContent:'start'}}>
-                    <Typography variant='h5' sx={{ fontWeight: 'bold' }}>
-                        Select sport you want to participate 
+        <Layout title="Team Register">
+            <Box sx={{ width: "100%" }}>
+                <Container
+                    sx={{
+                        height: "100%",
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "start",
+                    }}
+                >
+                    <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                        Select sport you want to participate
                     </Typography>
-                    <Box sx={{display:'flex', flexWrap:'wrap', gap:'.5em', padding: '1em 0'}}>
-                        {
-                            sports?.map(sport=> {
-                                let type = 'solo-registration'
-                                if(sport.type === 'team'){
-                                    type = 'team-registration'
-                                }
-                                if(sport.type === "duo"){
-                                    type = 'duo-registration' 
-                                }
-                                return (
-                                    <Link to={`/teamRegister/${type}`} key={sport.id} state={sport} style={{ textDecoration: 'none' }}>
-                                        <Item sx={{display:'grid', gridTemplateColumns:'1fr 2fr', placeItems:'center', padding: '.5em 1em' }} elevation={8}>
-                                            <Avatar src="https://media.istockphoto.com/photos/white-sphere-3d-render-on-background-picture-id1272466666?b=1&k=20&m=1272466666&s=170667a&w=0&h=OjcZdMXyoeRSmTzaQs7vgOZI_ks2YSzRQxh7KobCaq4=">
-                                                A
-                                            </Avatar>
-                                            <Typography variant="body1">{sport.name}</Typography>
-                                        </Item> 
-                                    </Link>
-                                )
-                            })
-                        }
+                    <Box
+                        sx={{
+                            display: "grid",
+                            gridTemplateColumns: {
+                                lg: "repeat(4,1fr)",
+                                xs: "repeat(2,1fr)",
+                                md: "repeat(4,1fr)",
+                            },
+                        }}
+                    >
+                        {sports?.map((sport) => {
+                            let type = "solo-registration";
+                            if (sport.type === "team") {
+                                type = "team-registration";
+                            }
+                            if (sport.type === "duo") {
+                                type = "duo-registration";
+                            }
+                            return (
+                            <Link
+                                to={`/teamRegister/${type}`}
+                                key={sport.id}
+                                state={sport}
+                                style={{ textDecoration: "none" }}
+                            >
+                            <CustomCard
+                                label={sport.name}
+                                image="https://www.pngkit.com/png/full/481-4812605_playing-cricket-cartoon-clipart-cricket-royalty-free-cricket.png"
+                            ></CustomCard>
+                            </Link>
+                            );
+                        })}
                     </Box>
                 </Container>
             </Box>
         </Layout>
-    )
-}
+    );
+};
 
-export default TeamRegister
+export default TeamRegister;
