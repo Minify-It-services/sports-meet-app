92a1bc141c48e60c42489dee7508874ba6f6d9a9
Salipa-Gurung <salipagurung@gmail.com>
2021-12-01 18:45:42 +0545

    notice crud integrated

--- src/admin/pages/notice/index.jsx
+++ src/admin/pages/notice/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
@@ -12,8 +12,10 @@ import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import Cookies from 'universal-cookie'
 
 import AddNotice from './AddNotice';
+import jsendDestructor from '../../../utils/api/jsendDestructor'
 
 // components
 import DrawerBar from '../../../components/DrawerBar';
@@ -33,48 +35,71 @@ const rows = [
 ];
 
 const Notice = () => {
+    const cookies = new Cookies()
+    const token = cookies.get('sports_app_token')
+    const jsendRes = new jsendDestructor({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+    })
     const [action, setaction] = useState(false);
     const [toEdit, settoEdit] = useState({});
+    const [notices, setNotices] = useState([]); 
     const editData=(row)=>{
         setaction(!action);
         settoEdit(row);
     }
-    const isObjEmpty=(obj)=>{
-        if (obj && Object.keys(obj).length === 0
-        && Object.getPrototypeOf(obj) === Object.prototype) {
-          return true;
+    const fetchNotices = async ()=>{
+        const response = await jsendRes.destructFromApi('/notices','GET')
+        if(response.status === 'success'){
+            setNotices(response.data)
         }
-        else return false;
-      }
+        else{
+            console.log(response.message);
+        }
+    }
+    useEffect(() => {
+       fetchNotices()
+    }, [])
+
+    const handleDelete =async (id)=> {
+        if(window.confirm('Are you sure you want to delete?')){
+            const response = await jsendRes.destructFromApi(`/notices/${id}`,'DELETE')
+            if(response){
+                console.log(response);
+            }
+            else{
+                window.location.reload()
+            }
+        }
+    }
+    
     return (
         <Box sx={{ display: 'flex' }}>
             <DrawerBar pageName={'Notice'} pageId ={5} />
             <Box sx={{flexGrow:1, pt:12.5, px:{xs:2,sm:3,md:5}}}>
                 <Box sx={{display:'flex', justifyContent:!action?'flex-end':'flex-start'}}>
-                    {!action?<Button variant="contained" color="primary" onClick={()=>{setaction(!action); if(!isObjEmpty(toEdit)){
-                        settoEdit({})
-                    }}}>Add Notice</Button>:<Button onClick={()=>setaction(!action)}> <ArrowBackIosIcon /></Button>}
+                    {!action?<Button variant="contained" color="primary" onClick={()=>{setaction(!action);}}>Add Notice</Button>:<Button onClick={()=>setaction(!action)}> <ArrowBackIosIcon /></Button>}
                 </Box>
                 {!action? <TableContainer component={Paper} sx={{mt:2}}>
                     <Table aria-label="simple table">
                         <TableHead>
                             <TableRow>
-                                <TableCell align="center">Id</TableCell>
+                                <TableCell align="center">S.N.</TableCell>
                                 <TableCell align="center">Title</TableCell>
                                 <TableCell align="center">Description</TableCell>
                                 <TableCell align="center">Actions</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {rows.map((row,index) => (
+                            {notices?.map((notice,index) => (
                                 <TableRow  key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                    <TableCell component="th" scope="row" align="center">{row.id}</TableCell>
-                                    <TableCell align="center">{row.title}</TableCell>
-                                    <TableCell align="center">{row.description}</TableCell>
+                                    <TableCell component="th" scope="row" align="center">{index+1}</TableCell>
+                                    <TableCell align="center">{notice.title}</TableCell>
+                                    <TableCell align="center">{notice.description}</TableCell>
                                     <TableCell align="center">
                                         <Stack direction="row" spacing={1} justifyContent="center" alignItems="center">
-                                        <Button key="one" variant="outlined" color="primary" onClick={()=>editData(row)}>Edit</Button>
-                                        <Button key="two" variant="outlined" color="error">Delete</Button>
+                                        <Button key="one" variant="outlined" color="primary" onClick={()=>editData(notice)}>Edit</Button>
+                                        <Button key="two" variant="outlined" color="error" onClick={()=>handleDelete(notice.id)}>Delete</Button>
                                         </Stack>
                                     </TableCell>
                                 </TableRow>
@@ -82,7 +107,7 @@ const Notice = () => {
                         </TableBody>
                     </Table>
                 </TableContainer>
-                : <AddNotice row={toEdit} changeAction={setaction}></AddNotice>
+                : <AddNotice row={toEdit} token={token} jsendRes={jsendRes} changeAction={setaction}></AddNotice>
                 }
             </Box>
         </Box>
