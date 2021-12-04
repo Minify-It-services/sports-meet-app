3ea8f22e83bd748853a6720f0ceb25e4f2c7e7d6
Alson33 <garbuja12alson@gmail.com>
2021-12-05 00:00:40 +0545

    add a back button for second pages

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,16 +1,20 @@
+import { ArrowBackIos } from '@mui/icons-material';
 import { Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom'
 
 // components
 import Navbar from '../components/Navbar';
 
-const Layout = ({ title, children }) => {
+const Layout = ({ title, children, isSecondPage=false }) => {
+    const navigate = useNavigate()
+
     return (
         <>
             <div style={{ height: '120px', width: '100%'}} />
                 {children}
             <div className="topbar" style={{backgroundColor:'#6F7CC7', color:'#FFFFFF'}}>
                 <Typography >
-                    {title}
+                    {isSecondPage&&<span style={{ marginRight: '1em', cursor: 'pointer' }} onClick={()=>navigate(-1)}><ArrowBackIos /></span>} {title}
                 </Typography>
             </div>
             <div style={{ height: '120px', width: '100%'}} />
