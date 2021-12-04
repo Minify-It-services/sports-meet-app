3841e405c099440b199a328ca872f293ce0a829d
Alson33 <garbuja12alson@gmail.com>
2021-12-04 21:51:02 +0545

    add indication for page in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,10 +40,10 @@ const Navbar = () => {
     return (
         <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
             <BottomNavigation sx={{ width:'100%' }} className="items" value={value}>
-                <BottomNavigationAction lable="Home" value="home" icon={<BiHomeAlt />} className="item" onClick={() => navigate('/')}/>
+                <BottomNavigationAction lable="Home" value="home" icon={<BiHomeAlt />} className={`item ${pathname==='/'?'item-active':''}`} onClick={() => navigate('/')} />
                 <BottomNavigationAction lable="Fixtures" value="fixtures" icon={<FiTablet />} className="item" />
-                <BottomNavigationAction lable="Register" value="register" icon={<HiOutlineDocumentAdd />} className="item" onClick={()=> navigate('/teamRegister')}/>
-                <BottomNavigationAction lable="Profile" value="profile" icon={<FiUser />} className="item" onClick={() => {navigate('/profile')}} />
+                <BottomNavigationAction lable="Register" value="register" icon={<HiOutlineDocumentAdd />} className={`item ${pathname==='/teamRegister'?'item-active':''}`} onClick={()=> navigate('/teamRegister')} />
+                <BottomNavigationAction lable="Profile" value="profile" icon={<FiUser />} className={`item ${pathname==='/profile'?'item-active':''}`} onClick={() => {navigate('/profile')}} />
             </BottomNavigation>
         </Paper>
     );
