b00a108c9936c17611bb8ae7df473fbd43a6780a
Alson33 <garbuja12alson@gmail.com>
2021-12-05 00:01:06 +0545

    add active in every page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,15 +21,17 @@ const Navbar = () => {
 
     const [value, setValue] = React.useState();
 
+    const path = pathname.split('/')
+
     React.useEffect(() => {
-        switch (pathname) {
-            case '/':
+        switch (path[1]) {
+            case '':
                 setValue('home')
                 break;
-            case '/profile':
+            case 'profile':
                 setValue('profile')
                 break;
-            case '/teamRegister':
+            case 'teamRegister':
                 setValue('register')
                 break;
             default: return
@@ -40,10 +42,10 @@ const Navbar = () => {
     return (
         <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
             <BottomNavigation sx={{ width:'100%' }} className="items" value={value}>
-                <BottomNavigationAction lable="Home" value="home" icon={<BiHomeAlt />} className={`item ${pathname==='/'?'item-active':''}`} onClick={() => navigate('/')} />
+                <BottomNavigationAction lable="Home" value="home" icon={<BiHomeAlt />} className={`item ${path[1]===''?'item-active':''}`} onClick={() => navigate('/')} />
                 <BottomNavigationAction lable="Fixtures" value="fixtures" icon={<FiTablet />} className="item" />
-                <BottomNavigationAction lable="Register" value="register" icon={<HiOutlineDocumentAdd />} className={`item ${pathname==='/teamRegister'?'item-active':''}`} onClick={()=> navigate('/teamRegister')} />
-                <BottomNavigationAction lable="Profile" value="profile" icon={<FiUser />} className={`item ${pathname==='/profile'?'item-active':''}`} onClick={() => {navigate('/profile')}} />
+                <BottomNavigationAction lable="Register" value="register" icon={<HiOutlineDocumentAdd />} className={`item ${path[1]==='teamRegister'?'item-active':''}`} onClick={()=> navigate('/teamRegister')} />
+                <BottomNavigationAction lable="Profile" value="profile" icon={<FiUser />} className={`item ${path[1]==='profile'?'item-active':''}`} onClick={() => {navigate('/profile')}} />
             </BottomNavigation>
         </Paper>
     );
