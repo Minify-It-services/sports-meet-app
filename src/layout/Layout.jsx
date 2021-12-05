2474b0d3fb10b2610399b01e7d0fb5b74ecccbf8
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2021-12-05 19:23:37 +0545

    fixing topbar height

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -10,15 +10,15 @@ const Layout = ({ title, children, isSecondPage=false }) => {
 
     return (
         <>
-            <div style={{ height: '12.5vh', width: '100%'}} />
+            <div style={{ height: '10vh', width: '100%'}} />
                 {children}
-            <div className="topbar" style={{backgroundColor:'#6F7CC7', color:'#FFFFFF', display:'flex', justifyContent:'space-between', alignItems:'center', height:'12.5vh'}}>
+            <div className="topbar" style={{backgroundColor:'#6F7CC7', color:'#FFFFFF', display:'flex', justifyContent:'space-between', alignItems:'center', height:'10vh'}}>
                 <Typography >
                     {isSecondPage&&<span style={{ marginRight: '1em', cursor: 'pointer' }} onClick={()=>navigate(-1)}><ArrowBackIos /></span>} {title}
                 </Typography>
                 <img src="./images/logo-full.png" alt="logo" style={{width:'50px'}} />
             </div>
-            <div style={{ height: '12.5vh', width: '100%'}} />
+            <div style={{ height: '10vh', width: '100%'}} />
             <Navbar />   
         </>
     )
