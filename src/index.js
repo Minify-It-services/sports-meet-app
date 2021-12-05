61ad1f0c64ab2f176522a8e74941197f4fe94a43
Yogesh070 <ythapa430@gmail.com>
2021-12-05 17:50:09 +0545

    change font to popins

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 // components:
 import App from './App';
 
+const theme = createTheme({
+  typography: {
+    fontFamily:  'Poppins, sans-serif',
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'Poppins', sans-serif;
+          font-style: normal;
+          font-display: swap;
+          font-weight: 400;
+          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+        }
+      `,
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
