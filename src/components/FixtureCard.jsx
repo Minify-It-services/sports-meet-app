487f84f0bf1af9a95eea3d3bbd3abde02786ab28
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2021-12-06 18:18:28 +0545

    fixing

--- src/components/FixtureCard.jsx
+++ src/components/FixtureCard.jsx
@@ -13,21 +13,38 @@ function FixtureCard() {
                 <Typography variant="subtitle2" gutterBottom component="h6" style={{fontWeight:'600' ,margin:0}}>Cricket</Typography>
             </div>
             <div className="fixture-container">
-            <Typography variant="subtitle1" gutterBottom component="div" className="header">Matches</Typography>
-            {/* //map div.fixtures for fixtures  */}
-            <div className="fixture">
-                <Typography variant="subtitle2" gutterBottom component="div">Team1</Typography>
-                <Typography variant="subtitle2" gutterBottom component="div" className="center">VS</Typography>
-                <Typography variant="subtitle2" gutterBottom component="div"className="right">Team2</Typography>
-            </div>
-            <div className="fixture">
-                <Typography variant="subtitle2" gutterBottom component="div">Team1</Typography>
-                <Typography variant="subtitle2" gutterBottom component="div" className="center">VS</Typography>
-                <Typography variant="subtitle2" gutterBottom component="div"className="right">Team2</Typography>
-            </div>
-            <div className="flex">
-                <Button variant="contained" size="small" style={{textTransform:"none"}} endIcon={<ArrowForwardIosIcon />}>Details</Button>
-            </div>
+                <Typography variant="subtitle1" gutterBottom component="div" className="header" sx={{fontWeight:'600'}}>
+                    Matches
+                </Typography>
+                {/* //map div.fixtures for fixtures  */}
+                <div className="fixture">
+                    <Typography variant="subtitle2" gutterBottom component="div">
+                        Software 2018
+                    </Typography>
+                    <Typography variant="subtitle2" gutterBottom component="div" className="center">
+                        VS
+                    </Typography>
+                    <Typography variant="subtitle2" gutterBottom component="div" className="right">
+                        Software 2019
+                    </Typography>
+                </div>
+                <div className="fixture">
+                    <Typography variant="subtitle2" gutterBottom component="div">
+                        Software 2018
+                    </Typography>
+                    <Typography variant="subtitle2" gutterBottom component="div" className="center">
+                        VS
+                    </Typography>
+                    <Typography variant="subtitle2" gutterBottom component="div" className="right">
+                        Software 2019
+                    </Typography>
+                </div>
+            
+                <div className="flex">
+                    <Button variant="contained" size="small" style={{textTransform:"none", fontSize:'0.85rem'}} endIcon={<ArrowForwardIosIcon />}>
+                        Details
+                    </Button>
+                </div>
             </div>
         </div>
     )
