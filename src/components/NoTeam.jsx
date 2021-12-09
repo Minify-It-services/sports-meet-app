3df964760c9f674225ac86f87d5964a7d80320b8
Alson33 <garbuja12alson@gmail.com>
2021-12-09 16:11:50 +0545

    custom no avaliable component

--- src/components/NoTeam.jsx
+++ src/components/NoTeam.jsx
@@ -4,11 +4,7 @@ import { Typography } from '@mui/material';
 
 const NoTeam = () => {
     return (
-        <Typography variant="h6" sx={{ textAlign: 'center' }}>
-            No team slot reamaning sorry. You can talk to your classmate.
-            <br />
-            If it is a system mistake you can contact 3rd year. Thank you
-        </Typography>
+        <Typography variant="body2" maxWidth="450px" margin="0 auto" textAlign="center" gutterBottom component="div" color='secondary'>No more team registration available for this sport.If it is a system error contact 3rd year. Thank you</Typography>
     )
 }
 
