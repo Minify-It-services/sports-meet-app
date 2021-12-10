dd55c650205ee4bd09831a51906434fe7eeda8fc
Alson33 <garbuja12alson@gmail.com>
2021-12-10 16:48:46 +0545

    add dynamic data to fixtures

--- src/components/FixtureCard.jsx
+++ src/components/FixtureCard.jsx
@@ -2,44 +2,49 @@ import React from 'react'
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import SportImage from '../assets/img/cricket.png'
 
 import './FixtureCard.scss'
-function FixtureCard() {
+function FixtureCard({ gameName, matches }) {
     return (
         <div id="fixture-card">
             <div className="sport-bar">
-                <img src={SportImage} alt="sporticon" style={{alignSelf:"center"}} />
-                <Typography variant="subtitle2" gutterBottom component="h6" style={{fontWeight:'600' ,margin:0}}>Cricket</Typography>
+                {/* <img src={sportImage} alt="sporticon" style={{alignSelf:"center"}} /> */}
+                <Typography variant="subtitle2" gutterBottom component="h6" style={{fontWeight:'600' ,margin:0}}>{gameName}</Typography>
             </div>
             <div className="fixture-container">
                 <Typography variant="subtitle1" gutterBottom component="div" className="header" sx={{fontWeight:'600'}}>
                     Matches
                 </Typography>
-                {/* //map div.fixtures for fixtures  */}
-                <div className="fixture">
-                    <Typography variant="subtitle2" gutterBottom component="div">
-                        Software 2018
-                    </Typography>
-                    <Typography variant="subtitle2" gutterBottom component="div" className="center">
-                        VS
-                    </Typography>
-                    <Typography variant="subtitle2" gutterBottom component="div" className="right">
-                        Software 2019
-                    </Typography>
-                </div>
-                <div className="fixture">
-                    <Typography variant="subtitle2" gutterBottom component="div">
-                        Software 2018
-                    </Typography>
-                    <Typography variant="subtitle2" gutterBottom component="div" className="center">
-                        VS
-                    </Typography>
-                    <Typography variant="subtitle2" gutterBottom component="div" className="right">
-                        Software 2019
-                    </Typography>
-                </div>
-            
+                {
+                    matches[0]&&(
+                        <div className="fixture">
+                            <Typography variant="subtitle2" gutterBottom component="div">
+                                {matches[0]?.team1?.name}
+                            </Typography>
+                            <Typography variant="subtitle2" gutterBottom component="div" className="center">
+                                {matches[0]?.score.team1Score} VS {matches[0]?.score.team2Score}
+                            </Typography>
+                            <Typography variant="subtitle2" gutterBottom component="div" className="right">
+                                {matches[0]?.team2?.name}
+                            </Typography>
+                        </div>
+                    )
+                }
+                {
+                    matches[1]&&(
+                        <div className="fixture">
+                            <Typography variant="subtitle2" gutterBottom component="div">
+                                {matches[1]?.team1?.name}
+                            </Typography>
+                            <Typography variant="subtitle2" gutterBottom component="div" className="center">
+                                {matches[1]?.score.team1Score} VS {matches[1]?.score.team2Score}
+                            </Typography>
+                            <Typography variant="subtitle2" gutterBottom component="div" className="right">
+                                {matches[1]?.team2?.name}
+                            </Typography>
+                        </div>
+                    )
+                }
                 <div className="flex">
                     <Button variant="contained" size="small" style={{textTransform:"none", fontSize:'0.85rem'}} endIcon={<ArrowForwardIosIcon />}>
                         Details
--- src/pages/Fixture.jsx
+++ src/pages/Fixture.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import FixtureCard from '../components/FixtureCard'
 import Layout from '../layout/Layout'
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
+import jsendDestructor from '../utils/api/jsendDestructor'
 
 function Fixture() {
+
+    const jsendRes = new jsendDestructor()
+    const [ matches, setMatches ] = useState([])
+
+    const getFixtures = async () => {
+        const { data, status, message } = await jsendRes.destructFromApi('/fixtures', 'GET')
+
+        if(status === 'success'){
+            setMatches(data);
+        }else{
+            console.log(data, message);
+        }
+    }
+
+    useEffect(() => {
+        getFixtures();
+    // eslint-disable-next-line
+    }, [])
     return (
         <Layout title="Fixture">
             <Box mx={{xs:'1rem', sm:'3rem', md:'5rem', lg:'9rem'}} my='1rem'>
-                <Typography color='primary' sx={{fontSize:'1rem', fontWeight:'600', }}>
-                    Team Games
-                </Typography>
-                <FixtureCard/>
-                <FixtureCard/>
-            </Box>
-            <Box mx={{xs:'1rem', sm:'3rem', md:'5rem', lg:'9rem'}} my='1rem'>
-                <Typography color='primary' sx={{fontSize:'1rem', fontWeight:'600', }}>
-                    Single Games
-                </Typography>
-                <FixtureCard/>
-                <FixtureCard/>
+                {
+                    matches?.map((match, i) => match[0]?.sport&&<FixtureCard key={i} gameName={match[0]?.sport.name} matches={match} />)
+                }
             </Box>
         </Layout>
     )
