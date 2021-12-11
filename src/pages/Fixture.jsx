b1459acecacf1001ac706966210b00a4ce372d92
Alson33 <garbuja12alson@gmail.com>
2021-12-11 12:09:48 +0545

    add loadings to home and fixture pages

--- src/pages/Fixture.jsx
+++ src/pages/Fixture.jsx
@@ -3,14 +3,17 @@ import FixtureCard from '../components/FixtureCard'
 import Layout from '../layout/Layout'
 import Box from '@mui/material/Box';
 
+import Loader from '../components/Loader'
 import jsendDestructor from '../utils/api/jsendDestructor'
 
 function Fixture() {
 
     const jsendRes = new jsendDestructor()
     const [ matches, setMatches ] = useState([])
+    const [ loading, setLoading ] = useState(false)
 
     const getFixtures = async () => {
+        setLoading(true)
         const { data, status, message } = await jsendRes.destructFromApi('/fixtures', 'GET')
 
         if(status === 'success'){
@@ -18,6 +21,7 @@ function Fixture() {
         }else{
             console.log(data, message);
         }
+        setLoading(false)
     }
 
     useEffect(() => {
@@ -26,6 +30,7 @@ function Fixture() {
     }, [])
     return (
         <Layout title="Fixture">
+            {loading&&<Loader />}
             <Box mx={{xs:'1rem', sm:'3rem', md:'5rem', lg:'9rem'}} my='1rem'>
                 {
                     matches?.map((match, i) => match[0]?.sport&&<FixtureCard key={i} gameName={match[0]?.sport.name} matches={match} />)
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import LinearProgress from '@mui/material/LinearProgress';
 
 // components
 import Layout from '../layout/Layout';
@@ -16,6 +17,7 @@ const Home = () => {
     const jsendRes = new jsendDestructor()
     const [notices, setNotices] = useState([])
     const [matches, setMatches] = useState([])
+    const [loading, setLoading] = useState(false)
 
     const getNotices = async () => {
         const response = await jsendRes.destructFromApi('/notices/today', 'GET')
@@ -28,6 +30,7 @@ const Home = () => {
     }
 
     const getMatches = async () => {
+        setLoading(true)
         const response = await jsendRes.destructFromApi('/matches/today', 'GET')
 
         if(response.status === 'success'){
@@ -35,6 +38,7 @@ const Home = () => {
         }else{
             console.log(response);
         }
+        setLoading(false)
     }
 
     useEffect(() => {
@@ -51,22 +55,26 @@ const Home = () => {
                     Matches
                 </Typography>
                 {
-                    matches.length===0?(
-                        <Typography color='primary' sx={{fontSize:'1rem', fontWeight: '500', padding:'10px 0px', textAlign: 'center'}}>No matches Today</Typography>
-                    ):(
-                        <Grid container spacing={2}>
-                            {
-                                matches?.map(match => (
-                                    <Grid item xs={12} sm={6} lg={4} key={match.id}>
-                                        <MatchCard 
-                                            match={match}
-                                            sportName={match.sport.name}
-                                        ></MatchCard>
-                                    </Grid>
-                                ))
-                            }
-                        </Grid>
-                    )
+                    loading?<LinearProgress color="inherit" />:(<>
+                        {
+                            matches.length===0?(
+                                <Typography color='primary' sx={{fontSize:'1rem', fontWeight: '500', padding:'10px 0px', textAlign: 'center'}}>No matches Today</Typography>
+                            ):(
+                                <Grid container spacing={2}>
+                                    {
+                                        matches?.map(match => (
+                                            <Grid item xs={12} sm={6} lg={4} key={match.id}>
+                                                <MatchCard 
+                                                    match={match}
+                                                    sportName={match.sport.name}
+                                                ></MatchCard>
+                                            </Grid>
+                                        ))
+                                    }
+                                </Grid>
+                            )
+                        }
+                    </>)
                 }
             </Box>
             {
