3e0e1fd828117beb82e54f5baf64b1715bbe164e
Alson33 <garbuja12alson@gmail.com>
2021-12-12 16:30:48 +0545

    add a regsiter time up component

---
+++ src/components/RegisterUp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Typography } from '@mui/material';
+
+const RegisterUp = () => {
+    return (
+        <Typography variant="body2" maxWidth="450px" margin="0 auto" textAlign="center" gutterBottom component="div" color='secondary'>Registration time has been closed. If you couldn't submit or update in teams due to Some reasons contact Us 3rd year, and we will register them. Thank You</Typography>
+    )
+}
+
+export default RegisterUp
